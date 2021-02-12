e8c59065dea65b41ed325522d4a9d5ae53d64e02
Belov Roman <rnbelov96@gmail.com>
2021-02-12 22:52:31 +0300

    Вносит правки

--- src/ts/calc.ts
+++ src/ts/calc.ts
@@ -1,14 +1,21 @@
 /* eslint-disable no-param-reassign */
 export {};
 
-const checkRangeEl = document.querySelector('.calc__range_check') as HTMLInputElement;
-const caseRangeEl = document.querySelector('.calc__range_case') as HTMLInputElement;
+const checkRangeEl = document.querySelector(
+  '.calc__range_check',
+) as HTMLInputElement;
+const caseRangeEl = document.querySelector(
+  '.calc__range_case',
+) as HTMLInputElement;
 
 let checkRangeValue = Number(checkRangeEl.value);
 let caseRangeValue = Number(caseRangeEl.value);
 let resultValue = checkRangeValue * caseRangeValue;
 
-const formatResult = (result: string) => `${result.substring(0, 1)} ${result.substr(1, 3)} ${result.substr(4, 3)} ₽`;
+const formatResult = (result: string) => `${result.substring(0, 1)}\xA0${result.substr(1, 3)}\xA0${result.substr(
+  4,
+  3,
+)} рублей`;
 
 const resultsElList = document.querySelectorAll('.calc__result-number');
 resultsElList.forEach(el => {
